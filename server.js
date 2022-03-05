cd666876a73658101e945630163c8f040cd54059
coltonhyer <coltonhyer@gmail.com>
2022-03-05 16:14:40 -0700

    Added specification changes to the PUT and DELETE to avoid altering the wrong pass type in the event that the user has more than one pass

--- server.js
+++ server.js
@@ -142,9 +142,9 @@ app.post('/passes', async (req, res) => {
 
 app.put('/passes/resident/:id',async (req, res) => {
     try{
-        let pass = await passModel.findOne({residentID: ObjectId(req.params.id)})
+        let pass = await passModel.findOne({residentID: ObjectId(req.params.id), passType: 'resident'})
         if (pass){
-            let updatedPass = await passModel.findOneAndUpdate({residentID: ObjectId(req.params.id)}, cleanPass(req.body))
+            let updatedPass = await passModel.findOneAndUpdate({residentID: ObjectId(req.params.id), passType: 'resident'}, cleanPass(req.body))
             res.status(200).send(updatedPass)
         }
         else{
