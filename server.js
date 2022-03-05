2880ddb0db015955cf976511c541c6fb22c383dd
coltonhyer <coltonhyer@gmail.com>
2022-03-05 16:06:41 -0700

    API Server. Written in express, this server will contain all of the logic for accessing the desired data. There may need to be a few tweaks as the front end gets created, but the core functionality is set

---
+++ server.js
@@ -0,0 +1,196 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const shajs  = require('sha.js')
+
+const logger = require('morgan'),
+    helmet = require('helmet'),
+    favicon = require('serve-favicon')
+const { ObjectId } = require('bson')
+
+var app = express()
+let loginModel, residentModel, passModel
+
+//put these at the external level so that you can use the key values for validations when creating
+let login = {
+    username: String,
+    password: String,
+    rold: String,
+    residentID:mongoose.ObjectId
+},
+resident = {
+    firstName: String,
+    lastName: String,
+    email: String,
+    phoneNumber: String,
+    apartment: String,
+    building: String,
+},
+pass = {
+    plateNum: String,
+    passType: String,
+    expiration: Date,
+    vehicleMake: String,
+    vehicleModel: String,
+    vehicleColor: String,
+    vehicleYear: String,
+    residentID: mongoose.ObjectId
+}
+
+const server = app.listen(3000, async () => {
+    console.log('Server listening on port 3000')
+    //connect to the database, set up the schemas, and create the models
+    try{
+        let db = await mongoose.connect('mongodb+srv://DBUser:rsOe3zE37DzffIY5@cluster0.bepto.mongodb.net/Parking?retryWrites=true&w=majority')
+        let loginSchema = new db.Schema(login)
+        let residentSchema = new db.Schema(resident)
+        let passSchema = new db.Schema(pass)
+        loginModel = db.model('logins', loginSchema)
+        residentModel = db.model('residents', residentSchema)
+        passModel = db.model('passes', passSchema)
+        console.log('Successfully connected to the database')
+    }
+    catch(err){
+        console.error(`Failed to connect to database: `, err)
+    }
+
+})
+
+app.use(helmet({
+    crossOriginEmbedderPolicy: true,
+    crossOriginOpenerPolicy: true,
+    crossOriginResourcePolicy: true,
+    originAgentCluster: true,
+    contentSecurityPolicy: true
+}))
+app.use(logger('dev'))
+//app.use(express.static('public'))
+//app.use(favicon())
+app.use(express.json())
+app.use(express.urlencoded())
+
+app.get('/logins', async (req, res) => {
+    let pwHash = hash(req.query.password)
+    try{
+        let login = await loginModel.findOne({username: req.query.username, password: pwHash})
+        if (login){
+            testID = login.residentID
+            let user = await residentModel.findOne({residentID: login.residentID})
+            if (user){
+                res.status(200).send(user)
+            }
+            else{
+                res.status(500).send('User not found')
+            }
+        }
+        else{
+            res.status(404).send('Login not found')
+        }
+    }
+
+    catch(err){
+        console.log('Error in getting logins: ')
+        console.error(err)
+    }
+})
+
+app.get('/passes', async (req, res) =>{
+    try{
+        let pass = await passModel.findOne({plateNum: req.query.plate})
+        if (pass){
+            res.status(200).send(pass)
+        }
+        else{
+            res.status(404).send('Pass not found')
+        }
+    }
+    catch(err){
+        console.log('Error in getting passes: ')
+        console.error(err)
+    }
+})
+
+app.get('/passes/:type/:id', async(req, res) => {
+    try{
+        let pass = await passModel.findOne({residentID: ObjectId(req.params.id), passType: req.params.type})
+        if (pass){
+            res.status(200).send(pass)
+        }
+        else{
+            res.status(404).send('Pass not found')
+        }
+    }
+    catch(err){
+        console.log('Error in getting pass: ')
+        console.error(err)
+    }
+})
+
+app.post('/passes', async (req, res) => {
+    if (!validatePass(req.body)){
+        res.status(400).send('Missing fields')
+        return
+    }
+    try{
+        let pass = await passModel.create(cleanPass(req.body))
+        res.status(200).send(pass)
+    }
+    catch(err){
+        console.log('Error in creating pass: ')
+        console.error(err)
+    }
+})
+
+app.put('/passes/resident/:id',async (req, res) => {
+    try{
+        let pass = await passModel.findOne({residentID: ObjectId(req.params.id)})
+        if (pass){
+            let updatedPass = await passModel.findOneAndUpdate({residentID: ObjectId(req.params.id)}, cleanPass(req.body))
+            res.status(200).send(updatedPass)
+        }
+        else{
+            res.status(404).send('Pass not found')
+        }
+    }
+    catch(err){
+        console.log('Error in updating pass: ')
+        console.error(err)
+    }
+})
+
+app.delete('/passes/:type/:id', async(req, res) => {
+    try{
+        let pass = await passModel.findOneAndDelete({residentID: ObjectId(req.params.id), passType: req.params.type})
+        if (pass){
+            res.status(200).send(pass)
+        }
+        else{
+            res.status(404).send('Pass not found')
+        }
+    }
+    catch(err){
+        console.log('Error in deleting pass: ')
+        console.error(err)
+    }
+})
+
+function hash(password){
+    return shajs('sha256').update(password).digest('hex')
+}
+
+function validatePass(pass){
+    let requiredFields = Object.keys(pass)
+    let missingFields = []
+    for (let field of requiredFields){
+        if (!pass[field]){
+            missingFields.push(field)
+        }
+    }
+
+    return !missingFields.length
+}
+
+function cleanPass(pass){
+    pass.date = new Date(pass.date)
+    pass.residentID = new ObjectId(pass.residentID)
+    return pass
+}
