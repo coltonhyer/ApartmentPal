5aae87c326592f31d3e45961127a8e81b1995ab8
coltonhyer <colton@passivelogic.com>
2022-03-07 17:36:35 -0700

    Login Component, this has been finished beyond API functionality and finishing touches on styling

--- public/components/login.js
+++ public/components/login.js
@@ -7,10 +7,34 @@ const Login = Vue.component('login', {
             <v-container id="loginMessage" fluid>
                 <div> Please login to continue. </div>
             </v-container>
-            <form id="loginForm">
-                <input></input>
-                <input></input>
-            </form>
+            <v-container id="loginContainer">
+                <v-form ref="form" id="loginForm">
+                    <v-text-field v-model="username" label="Username" :prepend-icon="'mdi-account'"></v-text-field>
+                    <v-text-field v-model="password" label="Password" class="shrink" :prepend-icon="'mdi-lock'" :type="show1 ? 'text': 'password'" ></v-text-field>
+                    <v-btn color="primary" @click="submit">Accept</v-btn>
+                </v-form>
+            </v-container>
         </div>
-    `
+    `,
+    data: function(){
+        return{
+            show1:false,
+            username:'',
+            password:''
+        }
+    },
+
+    methods:{
+        submit: function(){
+            console.log(`User logging in with the following information\nUsername: ${this.username}\nPassword:${this.password}`)
+           //this.$refs.form.$children[1].error = true
+            this.$root.$router.push('/lookup')
+        }
+    }
 })
+
+/*
+add these to the password field later:
+:append-icon="show1 ? 'mdi-eye': 'mdi-eye-off'" 
+@click:append="show1=!show1"
+*/
