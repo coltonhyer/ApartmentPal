5e3c854c169e914e9aee667d790ff543b07b3cb0
coltonhyer <colton@passivelogic.com>
2022-03-07 17:36:52 -0700

    beginning of lookup component

---
+++ public/components/lookup.js
@@ -0,0 +1,10 @@
+const Lookup = Vue.component('lookup', {
+    template:`
+        <div class="attendantLookup">
+            <v-container class="adminBanner" fluid>
+                <div> Attendant Hub </div>
+            </v-container>
+        </div>
+    `
+})
+
