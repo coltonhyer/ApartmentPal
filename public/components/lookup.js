089d1cd81d5e20ae9ed0ed17ea074a34b759e776
coltonhyer <10855410@uvu.edu>
2022-03-10 13:05:11 -0700

    lookupPage completed (visually)

--- public/components/lookup.js
+++ public/components/lookup.js
@@ -4,7 +4,20 @@ const Lookup = Vue.component('lookup', {
             <v-container class="adminBanner" fluid>
                 <div> Attendant Hub </div>
             </v-container>
+            <v-container id="lookupField">
+                <v-form ref="form" id="lookupForm">
+                    <v-text-field outlined style="min-width:80%;" label="License Plate Number" :prepend-icon="'mdi-car-side'" v-model="licensePlate"></v-text-field>
+                    <v-btn color="primary" @click="submit">Search</v-btn>
+                </v-form>
+            </v-container>
         </div>
     `
+
+    ,
+    methods:{
+        submit: function(){
+            this.$root.$router.push('/results')
+        }
+    }
 })
 
