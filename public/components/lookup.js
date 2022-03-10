dc333059848013f88b4388e77c44bdc608a14806
coltonhyer <10855410@uvu.edu>
2022-03-10 13:09:35 -0700

    WIP LookupPage statically working

--- public/components/lookup.js
+++ public/components/lookup.js
@@ -39,3 +39,28 @@ const Lookup = Vue.component('lookup', {
     }
 })
 
+const Results = Vue.component('results', {
+    //v-alert has many types, I'll use the base type for now, but adding dense and/or outlined is a solid choice
+    template:`
+        <div class="results">
+            <v-container class="adminBanner" fluid>
+                <div> Attendant Hub </div>
+            </v-container>
+            <v-container id="resultsField">
+                <v-alert text outlined dense type="warning" elevation=2>Expired Pass</v-alert>
+                <v-card elevation=3>
+                    <v-card-title> Colton's Card </v-card-title>
+                    <v-card-text><strong>Name:</strong> Colton</v-card-text>
+                    <v-card-text><strong>License Plate:</strong> ABC123</v-card-text>
+                    <v-card-text><strong>Date of Birth:</strong> 01/01/2000</v-card-text>
+                    <v-card-text><strong>Address:</strong> 123 Fake St</v-card-text>
+                    <v-card-text><strong>City:</strong> Fakeville</v-card-text>
+                    <v-card-text><strong>State:</strong> Fake</v-card-text>
+                    <v-card-text><strong>Zip Code:</strong> 12345</v-card-text> 
+                </v-card>
+            </v-container>
+            <v-btn bottom absolute right color="error">Print Ticket</v-btn>
+        </div>
+    `
+})
+
