c2a1dfa284b1da74a040fe5546d64c23d7b1d24e
coltonhyer <10855410@uvu.edu>
2022-03-13 11:20:27 -0600

    dynamic login completed

--- public/components/login.js
+++ public/components/login.js
@@ -9,8 +9,8 @@ const Login = Vue.component('login', {
             </v-container>
             <v-container id="loginContainer">
                 <v-form ref="form" id="loginForm">
-                    <v-text-field v-model="username" label="Username" :prepend-icon="'mdi-account'"></v-text-field>
-                    <v-text-field v-model="password" label="Password" class="shrink" :prepend-icon="'mdi-lock'" :type="show1 ? 'text': 'password'" ></v-text-field>
+                    <v-text-field @blur="reset" @input="reset" v-model="username" :rules="unRules" label="Username" :prepend-icon="'mdi-account'" required></v-text-field>
+                    <v-text-field @blur="reset" @input="reset" v-model="password" :rules="pwRules" label="Password" class="shrink" :prepend-icon="'mdi-lock'" :type="show1 ? 'text': 'password'" required></v-text-field>
                     <v-btn color="primary" @click="submit">Accept</v-btn>
                 </v-form>
             </v-container>
@@ -20,15 +20,43 @@ const Login = Vue.component('login', {
         return{
             show1:false,
             username:'',
-            password:''
+            password:'',
+            passwordValid: true,
+            unRules:[
+                v => !!v || 'Username is required',
+                v => this.passwordValid 
+            ],
+            pwRules:[
+                v => !!v || 'Password is required',
+                v => this.passwordValid || 'Invalid username/password'
+            ]
         }
     },
 
     methods:{
-        submit: function(){
-            console.log(`User logging in with the following information\nUsername: ${this.username}\nPassword:${this.password}`)
+        submit: async function(){
+            this.$refs.form.resetValidation()
+            await axios.get(`/logins?username=${this.username}&password=${this.password}`).then((res) =>{
+                let user = res.data.user
+                let role = res.data.role
+                if (role==="resident"){
+                    this.$root.$router.push('/resident')
+                }
+                else if (role === "admin"){
+                    this.$root.$router.push('/lookup')
+                }
+                this.passwordValid=true
+                this.$refs.form.reset()
+            }).catch(err =>{
+                this.passwordValid = false
+                this.$refs.form.validate()
+                console.log(err.response)
+            })
            //this.$refs.form.$children[1].error = true
-            this.$root.$router.push('/lookup')
+            //this.$root.$router.push('/lookup')
+        },
+        reset: function(){
+            this.$refs.form.resetValidation()
         }
     }
 })
