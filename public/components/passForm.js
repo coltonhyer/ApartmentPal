f2fb7913cf53b861a681085d449339287e57a41a
coltonhyer <10855410@uvu.edu>
2022-04-11 16:42:56 -0600

    Still applies to last commit

--- public/components/passForm.js
+++ public/components/passForm.js
@@ -80,6 +80,9 @@ const Form = Vue.component('pass-form',{
             expDate.setDate(expDate.getDate()-21)
             return today > expDate
         },
+        goToHome: function(){
             this.$refs.form.reset()
+            this.$root.$router.push('/resident')
+        }
     }
 })
